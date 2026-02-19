6d85ae2038950f57a63df43b88840f91fbf6d852
Jules Novoa <juno9637@colorado.edu>
2026-02-19 16:38:42 -0700

    Debug UI and Atlas Model

--- public/js/cube.js
+++ public/js/cube.js
@@ -1,5 +1,24 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import {OBJLoader} from 'three/addons/loaders/OBJLoader.js';
+import GUI from "https://cdn.jsdelivr.net/npm/lil-gui@0.21.0/dist/lil-gui.esm.js";
+
+const gui = new GUI({
+    width: 340,
+    title: 'Debug Menu',
+    closeFolders: false
+})
+//gui.close()
+//gui.hide()
+
+window.addEventListener('keydown', (event) => {
+    if(event.key === 'h') {
+        gui.show(gui._hidden)
+    }
+})
+
+const debugObject = {}
+debugObject.color = '#d6d8e9'
 
 const app = document.getElementById("app");
 
@@ -12,11 +31,52 @@ const canvas = document.querySelector('canvas.webgl')
 //scene
 const scene = new THREE.Scene();
 
-//object
-const geometry = new THREE.BoxGeometry(1,1,1)
-const material = new THREE.MeshBasicMaterial({color: 0xC5D4D6})
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+// Light
+const ambientLight = new THREE.AmbientLight(0xffffff, 1.5);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+scene.add(directionalLight);
+
+//atlas
+const objLoader = new OBJLoader();
+const AtlasMaterial = new THREE.MeshStandardMaterial({
+    color: debugObject.color,
+    metalness: 0.2,
+    roughness: 0.7,
+});
+
+debugObject.positionY = -.3
+
+gui
+    .addColor(debugObject, 'color')
+    .onChange( () => {
+        AtlasMaterial.color.set(debugObject.color)
+    })
+
+gui
+    .add(debugObject, 'positionY')
+    .min(-3)
+    .max(3)
+    .step(0.01)
+    .name('elevation')
+
+objLoader.load(
+    'models/OBJATLAS.obj',
+    (object) => {
+        console.log('OBJ loaded successfully', object);
+        object.traverse((child) => {
+            if (child.isMesh) {
+                child.material = AtlasMaterial;
+                child.position.y = debugObject.positionY
+            }
+        });
+        object.position.z = -.1;
+        object.rotation.y = Math.PI;
+
+        scene.add(object);
+    }
+);
 
 //object
 const circleGeometry = new THREE.CircleGeometry(1.2,64)
@@ -27,9 +87,10 @@ const circleMaterial = new THREE.MeshBasicMaterial({
     side: THREE.DoubleSide
 });
 const circleMesh = new THREE.Mesh(circleGeometry, circleMaterial)
-circleMesh.rotation.x = -Math.PI / 2;
-circleMesh.position.y = -0.5;
 
+circleMesh.scale.set(1.2, 1.2, 1.2)
+circleMesh.rotation.x = -Math.PI / 2;
+circleMesh.position.y = -.35
 
 scene.add(circleMesh)
 
@@ -40,7 +101,8 @@ const sizes = {
 }
 //camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height)
-camera.position.z = 3
+camera.position.z = 2
+camera.position.y = .5
 scene.add(camera)
 
 //renderer
